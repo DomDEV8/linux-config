f875d26147bdd32911441a439102b83e568b7e4c
DARAEDom <4romad67@solent.ac.uk>
2021-08-27 12:31:59 +0100

    void

---
+++ void_x86_setup.sh
@@ -0,0 +1,22 @@
+PKG = {
+'mesa-dri'
+'vulkan-loader'
+'mesa-vulkan-radeon'
+'mesa-vaapi'
+'mesa-vdpau'
+'curl'
+'wget'
+'neovim'
+'lua'
+'kitty'
+'zsh'
+}
+
+sudo xbps-install -Su
+
+for PKG in "${PKGS[@]}"; do
+    echo "INSTALLING: ${PKG}"
+    sudo xbps-install "$PKG" -y
+done
+
+echo 'Finished!'
